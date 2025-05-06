2c44e4780e8ef1ab4565596224abb76c0daac3b1
Caio Souza <ocaiomoises@gmail.com>
2025-05-06 17:35:45 -0300

    Initial Config

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+namespace binary_database;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Write("Hello World!");
+    }
+}
